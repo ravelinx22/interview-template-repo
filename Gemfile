4316ed1fd12cdee75e7383420329270f694b87ed
William Ravelo M <drummerwilliam@gmail.com>
2022-05-09 23:31:27 -0500

    added pagination

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'httparty'
 gem 'dotenv-rails'
 gem "active_model_serializers"
+gem 'will_paginate', '~> 3.3'
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,18 @@
 module Api
   class UsersController < ApiApplicationController
     def index
-      render json: User.all, each_serializer: UserSerializer
+      render json: User.paginate(
+        per_page: pagination_params[:per_page] || 10,
+        page: pagination_params[:page] || 1
+      ), each_serializer: UserSerializer
+    end
+
+    private
+
+    def pagination_params
+      params.permit(
+        :per_page, :page
+      )
     end
   end
 end
