aab839c13b7be8df43810c063226104a184578f5
William Ravelo M <drummerwilliam@gmail.com>
2022-05-09 23:33:42 -0500

    added filter

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,19 @@
 module Api
   class UsersController < ApiApplicationController
     def index
-      render json: User.paginate(
+      render json: User.where(filter_params).paginate(
         per_page: pagination_params[:per_page] || 10,
         page: pagination_params[:page] || 1
       ), each_serializer: UserSerializer
     end
 
     private
+
+    def filter_params
+      params.permit(
+        :status
+      )
+    end
 
     def pagination_params
       params.permit(
