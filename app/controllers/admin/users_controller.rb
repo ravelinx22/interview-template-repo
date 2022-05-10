afd394f074db3112b1d3676b9fdf7799317856b4
William Ravelo M <drummerwilliam@gmail.com>
2022-05-10 10:41:13 -0500

    detail button

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,28 @@
 module Admin
   class UsersController < AdminApplicationController
     def index
+      @users = filter_params[:status].blank? ? User : User.where(filter_params)
+      @users = @users.paginate(
+        page: paginate_params[:page], per_page: 10
+      )
     end
 
     def show
       @name = "James"
+    end
+
+    private
+    
+    def filter_params
+      params.permit(
+        :status
+      )
+    end
+
+    def paginate_params
+      params.permit(
+        :page
+      )
     end
   end
 end
